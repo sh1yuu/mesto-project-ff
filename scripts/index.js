352419d294379ed2300cc8ef72c9875d8c998038
Misha Shukanov <zaline1337@yandex.by>
2023-12-07 00:00:50 +0300

    добавил карточки на страницу и сделал функцию удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function cardsAdd(name, link, cardDelete) {
+  const cards = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cards.querySelector('.card__delete-button');
+  cards.querySelector('.card__title').textContent = name;
+  cards.querySelector('.card__image').src = link;
+  deleteButton.addEventListener('click', (event => cardDelete(event)));
+
+  return cards;
+}
 
 // @todo: Функция удаления карточки
+function cardDelete(event) {
+  const listItem = event.target.closest('.card');
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(item => placesList.append(cardsAdd(item.name, item.link, cardDelete)));
