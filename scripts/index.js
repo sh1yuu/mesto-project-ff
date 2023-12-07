2b6449226b859ac381a59368a8bbb64bd797ea67
Misha Shukanov <zaline1337@yandex.by>
2023-12-07 14:47:43 +0300

    добавил атрибут alt к изображениям

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,13 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function cardsAdd(name, link, cardDelete) {
+function cardsAdd(name, link, callback) {
   const cards = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cards.querySelector('.card__delete-button');
   cards.querySelector('.card__title').textContent = name;
   cards.querySelector('.card__image').src = link;
-  deleteButton.addEventListener('click', (event => cardDelete(event)));
+  cards.querySelector('.card__image').setAttribute('alt', 'картинка с изображением природы');
+  deleteButton.addEventListener('click', (event => callback(event)));
 
   return cards;
 }
